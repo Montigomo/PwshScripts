c4a3039c37d77ebdafac5302dfc55b7e4f8ac655
Montigomo <agitech@gmail.com>
2017-02-10 14:53:42 +0300

    2

--- Scripts/Get-NetTools.ps1
+++ Scripts/Get-NetTools.ps1
@@ -1,6 +1,4 @@
 ﻿
-#[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
-#[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")  
 Add-Type -AssemblyName PresentationFramework
 
 #Build the GUI
@@ -9,10 +7,11 @@ Add-Type -AssemblyName PresentationFramework
     xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
     xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
     x:Name="Window" Title="Putty helper" WindowStartupLocation = "CenterScreen" ResizeMode="NoResize"
-    Width = "300" Height = "125" ShowInTaskbar = "True" Background = "lightgray"> 
+    Width = "300" Height = "150" ShowInTaskbar = "True" Background = "lightgray"> 
     <StackPanel >
         <ComboBox x:Name="combobox" Margin="10"/>
         <Button x:Name="buttonConnect" Content="CONNECT" Height="25" Width="90"/>
+        <Button x:Name="buttonPagent" Content="Pagent" Height="25" Width="90" Margin="5,10"/>
     </StackPanel>
 </Window>
 "@
@@ -25,17 +24,13 @@ $puttyItems = @{
     'eomy' = @{'Path' = $puttyPath; 'Args' = "-load eomy -l root"}
 };
 
-#Start-Process $puttyPath -ArgumentList ("-load asus")
-#Start-Process $puttyPath -ArgumentList ("-load eomy -l root") 
 
 $reader=(New-Object System.Xml.XmlNodeReader $xaml)
 $Window=[Windows.Markup.XamlReader]::Load( $reader )
-
 $buttonConnect = $Window.FindName('buttonConnect');
+$buttonPagent = $Window.FindName('buttonPagent');
 $combobox = $Window.FindName('combobox')
-
-
-$combobox.SelectedItem = 'eomy'
+$combobox.SelectedIndex = 0 ;#'eomy'
 
 foreach($actItem in $puttyItems.GetEnumerator())
 {
@@ -47,8 +42,23 @@ $buttonConnect.Add_Click({
     $action = $puttyItems.Get_Item($combobox.SelectedItem.ToString());
     $pathi = $action['Path'];
     $argsi = $action['Args'];
-    Start-Process $pathi -ArgumentList $argsi
+    if($argsi)
+    {
+        Start-Process $pathi -ArgumentList $argsi
+    }
+    else
+    {
+        Start-Process $pathi
+    }
+
     $Window.Close();
 })
+
+$buttonPagent.Add_Click({
+    Start-Process 'D:\tools\network\PuTTY\PAGEANT.EXE' -ArgumentList 'D:\tools\network\PuTTY\keys\asus\private.ppk D:\tools\network\PuTTY\keys\eomy\private.ppk'
+})
+
+
+
 
 $Window.ShowDialog() | Out-Null
