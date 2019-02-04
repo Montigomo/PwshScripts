985fb5a6ee00644cd63c6ac7421a27a911a5cc66
agitech <agitech@gmail.com>
2019-02-04 19:31:04 +0300

    some

--- Scripts/Get-NetTools.ps1
+++ Scripts/Get-NetTools.ps1
@@ -22,10 +22,20 @@ $pgAgent = 'D:\tools\network\PuTTY\PAGEANT.EXE'
 
 $puttyItems = @{
     'WinSCP' = @{'Path' = "D:\tools\__this\WinSCP.lnk"; 'Args' = ""};
-    'asus router' =  @{'Path' = $puttyPath; 'Args' = "-load asus"} ;
-    'eomy' = @{'Path' = $puttyPath; 'Args' = "-load eomy -l root"};
-    'ubnt' = @{'Path' = $puttyPath; 'Args' = "-load ubnt -l ubnt"}
+    #'asus router' =  @{'Path' = $puttyPath; 'Args' = "-load asus"} ;
+    #'eomy' = @{'Path' = $puttyPath; 'Args' = "-load eomy"};
+    #'ubnt' = @{'Path' = $puttyPath; 'Args' = "-load ubnt"}
 };
+
+$values = get-childitem -Path "HKCU:\Software\SimonTatham\PuTTY\Sessions" | Select -Property Name | Foreach {$_.Name} | Foreach {
+    if($_.LastIndexOf("\") -ne -1){ $_.Substring($_.LastIndexOf("\") + 1);   }
+}
+
+foreach($item in $values)
+{
+    $puttyItems.Add($item, @{'Path' = $puttyPath; 'Args' = "-load $item"});
+
+}
 
 
 $reader=(New-Object System.Xml.XmlNodeReader $xaml)
