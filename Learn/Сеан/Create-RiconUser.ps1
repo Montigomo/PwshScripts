d90e657264a6013f2c89296cd5b3b56135ccf28e
agitech <agitech@gmail.com>
2021-12-31 06:31:42 +0300

    123

--- Learn/Сеан/Create-RiconUser.ps1
+++ Learn/Сеан/Create-RiconUser.ps1
@@ -1,4 +1,4 @@
-# Скрипт для созданя папки куда сканер Ricon будет отправлять файлы
+# Скрипт для созданя папки для Ricon
 
 #New-localUser -Name "RiconUser" -Description "RiconUser" -NoPassword
 <# Список служб
@@ -6,11 +6,12 @@ fdPHost
 #>
 
 # Инициалтзация переменых
-$UserName = "12345"
-$Password = "12345"
+$UserName = "RiconUser"
+$Password = "riconuser"
 $Description = "RiconUser"
 $DeskTopFolder = [Environment]::GetFolderPath("Desktop");
-$RiconScanFolder = "$DeskTopFolder\RiconScan"
+$RiconFolderName = "RiconScan"
+$RiconScanFolder = "$DeskTopFolder\$RiconFolderName"
 $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
 
 # Проверка наличия и моздание в случае отсутствия папки C:\Users\[CurrentUser]\Desctop\RiconScan
@@ -19,18 +20,66 @@ if(!(Test-Path $RiconScanFolder))
     New-Item -Path $RiconScanFolder -ItemType Directory -Force
 }
 
-#Set-LocalUser -Name "12345" -Password $SecurePassword
-#Exit
+# Проверка наличия учетной записи
+if(!(Get-LocalUser -Name $UserName -ErrorAction SilentlyContinue))
+{
+    New-LocalUser -Name $UserName -Description $Description -Password $SecurePassword
+}
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+Set-LocalUser -Name $UserName -Password $SecurePassword
 
-#$Password = "riconuser"
+# Предоставление общего доступа к папке
+if(!(Get-SmbShare $RiconFolderName -ErrorAction SilentlyContinue))
+{
+    New-SmbShare -Name $RiconFolderName -Path $RiconScanFolder -FullAccess "RiconUser"
+}
+
+$acim = Get-SmbShareAccess $RiconFolderName
+$flag = $true
+foreach($item in $acim)
+{
+    $accounName = $item.CimInstanceProperties["AccountName"].Value
+    # 2 - read, 0 - full
+    $accessRights = $item.CimInstanceProperties["AccessRight"].Value
+    if($accounName.EndsWith($UserName) -and $accessRights -eq 0)
+    {
+        $flag = $false
+        break
+    }
+}
+
+if($flag)
+{
+    Grant-SmbShareAccess -Name $RiconFolderName -AccountName "RiconUser" -AccessRight Full -Force
+}
+
+# Настройка параметров общего доступа к сети
 
 
-#$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
 
-exit
-$c = get-credential -UserName 12345
-$SecurePassword = $c.Password
-$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
-$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-New-LocalUser -Name $UserName -Description $Description -Password $SecurePassword
+# Внесение учетной записи пользователя в адресную книгу Ricon
+
+
+
+# Проверка наличия, режима запуска и включие необходимых служб
+# SSDP Discovery
+# UPnP Device Host
+# Список служб
+$items = @("ssdpsrv")
+foreach($item in $items)
+{
+    if(($service = Get-Service -Name $item -ErrorAction SilentlyContinue))
+    {
+        # ($service.StartType) -eq [System.ServiceProcess.ServiceStartMode]::Manual 
+        if($service.StartType -ne [System.ServiceProcess.ServiceStartMode]::Automatic)
+        {
+            $service | Set-Service -StartupType ([System.ServiceProcess.ServiceStartMode]::Automatic)
+        }
+        if($service.Status -ne "Running")
+        {
+            $service | Start-Service
+        }
+    }
+}
+
