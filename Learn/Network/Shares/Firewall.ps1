9156e428dfbfbce90e6f159c2aa648692df89f1c
Agitech <agitech@gmail.com>
2022-06-16 12:59:20 +0300

    321

--- Learn/Network/Shares/Firewall.ps1
+++ Learn/Network/Shares/Firewall.ps1
@@ -1,12 +1,11 @@
 
 
+
 netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=Yes
 
 netsh advfirewall firewall set rule group=”network discovery” new enable=yes
 
-
 netsh firewall set service type=fileandprint mode=enable profile=all
-
 
 Get-NetFirewallRule -DisplayGroup 'Network discovery' | Set-NetFirewallRule -Profile 'Private, Domain' -Enabled true
 
