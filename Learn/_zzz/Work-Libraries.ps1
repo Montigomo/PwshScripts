ef11647b5da49ec531875ac8fe5ae66c4824406e
agitech <agitech@gmail.com>
2022-07-06 11:02:33 +0300

    123

--- Learn/_zzz/Work-Libraries.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-# Enumarate existing libraries
-
-Add-type -path Microsoft.WindowsAPICodePack.Shell.dll
-
-[Microsoft.WindowsAPICodePack.Shell.KnownFolders]::Libraries | Select-Object Name,ParsingName
--- Learn/_zzz/asus.restart.ps1
+++
@@ -1,4 +0,0 @@
-
-Get-Telnet -RemoteHost "192.168.1.1" -Commands "admin","ajhtcn17ufvg91","reboot"
-
-sleep -Seconds 5
