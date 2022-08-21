b14d405cc5af96876433a1d6e5cc6bc6235f37c8
agitech <agitech@gmail.com>
2022-08-21 19:02:22 +0300

    123

--- Scripts/Usefull Scripts/Set-AtStartup.ps1
+++ Scripts/Usefull Scripts/Set-AtStartup.ps1
@@ -10,15 +10,11 @@ function Set-AtStartup {
     #$P = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
     $P = New-ScheduledTaskPrincipal -GroupId "S-1-5-32-544" -RunLevel Highest
     $S = New-ScheduledTaskSettingsSet
-    $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
-    Register-ScheduledTask T1 -InputObject $D
-
-
+    $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S -
+    Register-ScheduledTask -TaskPath "T1" -InputObject $D -TaskName "AtStartup"
 
     # $trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
-
     # Register-ScheduledJob -Trigger $trigger -FilePath C:\fso\Get-BatteryStatus.ps1 -Name GetBatteryStatus
-
 }
 
 Set-AtStartup -ProgrammPath "D:\tools\network\Virtual Here\vhui64.exe"
