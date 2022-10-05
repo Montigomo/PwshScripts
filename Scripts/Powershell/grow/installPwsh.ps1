9593411baa2878bd718f060eee8651a88389de5a
agitech <agitech@gmail.com>
2022-10-05 14:59:06 +0300

    123

--- Scripts/Powershell/grow/installPwsh.ps1
+++ Scripts/Powershell/grow/installPwsh.ps1
@@ -1,5 +1,13 @@
 
-#+++ import modules
+
+$Logfile = "$PSScriptRoot\cupdater.log"
+
+function WriteLog {
+    Param ([string]$LogString)
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $LogMessage = "$Stamp $LogString"
+    Add-content $LogFile -value $LogMessage
+}
 
 $modulePathBase = "$PSScriptRoot\..\..\..\Modules"
 
@@ -15,11 +23,29 @@ foreach($path in $pathArray)
     }
 }
 
-Get-IsAdmin
-Install-Far
-Install-Powershell
-Install-OpenSsh 
-Set-OpenSsh
+
+
+if(Get-IsAdmin)
+{
+    try{
+        Write-Host "Runned as admin"
+        Write-Host "Installing far ..."
+        Install-Far
+        Write-Host "Installing pwsh ..."
+        Install-Powershell
+        Write-Host "Installing ssh ..."
+        Install-OpenSsh 
+        Write-Host "Config ssh ..."
+        Set-OpenSsh
+    }catch{
+        WriteLog "GetFiles Error: $_"
+        exit
+    }
+}
+else {
+    Write-Host "Script worked correctly only in admin mode."
+    exit
+}
 
 Write-Host -NoNewLine 'All task completed successfully...';
 #$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
