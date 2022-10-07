7c34a4ea6ce16d418e1c17693e5f2c3a39c99bc0
agitech <agitech@gmail.com>
2022-10-07 13:55:15 +0300

    123

--- Scripts/Powershell/grow/installPwsh.ps1
+++ Scripts/Powershell/grow/installPwsh.ps1
@@ -1,7 +1,10 @@
 
 
 $Logfile = "$PSScriptRoot\cupdater.log"
-
+       
+$sshPublicKeys = @(
+    "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAiHq57Mo7efkA05q33JkdZ9g96VE4TjCn8lW0jZxn+n0TzkmlNZEi1E6fbfRSv3iK2XnNBFbOUBLinnMtzmDIAbez0FjKJOSyEk3ZvhD6QAvWh4UW77udzr1V9BROKDbe0ZpkHBHs4nc1LrjZ7+oAVnOHDpYa8FQh/jPf77js11YdNrrPbxi2Gg9SLpcDN6b8L88/eebWDaGNYzKw534eY7JT7FTUwcpAd0krfyh7h99pGJaWtzvwsot/ntQE0QiCmu2IXIYXz0iKBuI38PD9AAR3l7vsOzHIkWqcTRhNsfcrlvST8lZcrlOfwdK8peu1RGRegvWeL8tvunAd9rjBNQ== agitech"
+)
 function WriteLog {
     Param ([string]$LogString)
     Write-Host $LogString
@@ -24,8 +27,6 @@ foreach($path in $pathArray)
     }
 }
 
-
-
 if(Get-IsAdmin)
 {
     try{
@@ -37,7 +38,35 @@ if(Get-IsAdmin)
         WriteLog "Installing ssh ..."
         Install-OpenSsh 
         WriteLog "Config ssh ..."
-        Set-OpenSsh
+        
+        Set-OpenSSH -PublicKeys $sshPublicKeys -DisablePassword $true
+
+         # DNS Client
+        # Function Discovery Provider Host
+        # Function Discovery Resource Publication
+        # HomeGroup Provider
+        # HomeGroup Listener
+        # Peer Networking Grouping
+        # SSDP Discovery
+        # UPnP Device Host
+        
+        $items = @("dnscache", "fdphost", "FDResPub", "p2psvc", "ssdpsrv", "upnphost")
+        foreach($item in $items)
+        {
+            if(($service = Get-Service -Name $item -ErrorAction SilentlyContinue))
+            {
+                # ($service.StartType) -eq [System.ServiceProcess.ServiceStartMode]::Manual 
+                if($service.StartType -ne [System.ServiceProcess.ServiceStartMode]::Automatic)
+                {
+                    $service | Set-Service -StartupType ([System.ServiceProcess.ServiceStartMode]::Automatic)
+                }
+                if($service.Status -ne "Running")
+                {
+                    $service | Start-Service
+                }
+            }
+        }       
+
     }catch{
         WriteLog "GetFiles Error: $_"
         exit
