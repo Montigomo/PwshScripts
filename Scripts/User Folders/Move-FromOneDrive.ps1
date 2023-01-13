fc3401c457d92ec3fe79f1fbc540c1546e1cd031
agitech <agitech@gmail.com>
2023-01-13 09:02:35 +0300

    123

--- Scripts/User Folders/Move-FromOneDrive.ps1
+++ Scripts/User Folders/Move-FromOneDrive.ps1
@@ -3,6 +3,8 @@ param (
     [Parameter()][string]$UserProfilesFolder = $env:USERPROFILE
 )
 
+$debug = ($DebugPreference -eq "SilentlyContinue") -or ($PSBoundParameters.Debug.IsPresent) -or ($PSDebugContext)
+
 $userFolders = @{
     "Documents" = @{Handle = $false; FolderName = "Personal"; FolderClass = "{f42ee2d3-909f-4907-8871-4c22fc0bf756}" };
     "Pictures" = @{Handle = $true; FolderName = "Pictures"; FolderClass = "{0DDD015D-B06C-45D5-8C4C-F59713854639}" };
@@ -11,8 +13,10 @@ $userFolders = @{
     "Music" = @{Handle = $false; FolderName = "Music"; FolderClass = "{A0C69A99-21C8-4671-8703-7934162FCF1D}" };
 }
 
-#Stop-Process -ProcessName explorer -Force
-#Start-Sleep -Seconds 2
+if(-not $debug){
+    Stop-Process -ProcessName explorer -Force
+    Start-Sleep -Seconds 2
+}
 
 foreach($key in $userFolders.Keys)
 {
@@ -27,11 +31,11 @@ foreach($key in $userFolders.Keys)
             New-Item $UserFolderPath -ItemType Directory -ErrorAction SilentlyContinue -Force
         }
 
-        Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" 
+        Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" `
             -Name $UserFolderName -Value $UserFolderPath -Type String -Force
-        Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" 
+        Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" `
             -Name $UserFolderClass -Value $UserFolderPath -Type ExpandString
-        Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" 
+        Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" `
             -Name $UserFolderName -Value $UserFolderPath -Type ExpandString
 
         #attrib +r -s -h "%USERPROFILE%\Documents" /S /D
