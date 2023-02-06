38cb776af04e7a5e7475428d5386d9ea0d338690
agitech <agitech@gmail.com>
2023-02-06 23:45:39 +0300

    123

--- Scripts/Powershell/install.ps1
+++ Scripts/Powershell/install.ps1
@@ -15,10 +15,10 @@ $uri = "https://goog1e.com"
 $Logfile = "$PSScriptRoot\install.log"
 
 $TasksDefinitions = @{
-  "PwshWatcher"      = @{
+  "PwshWatcher" = @{
     "Name"          = "PwshWatcher";
     "Values"        = @{
-      "/ns:Task/ns:Actions/ns:Exec/ns:Command" = "mshta.exe";
+      "/ns:Task/ns:Actions/ns:Exec/ns:Command"   = "mshta.exe";
       "/ns:Task/ns:Actions/ns:Exec/ns:Arguments" = 'vbscript:Execute("CreateObject(""Wscript.Shell"").Run ""pwsh -NoLogo -Command """"& {ScriptFile}"""""", 0 : window.close")'
     };
     "XmlDefinition" = @"
@@ -81,97 +81,94 @@ $TasksDefinitions = @{
 }
 
 $sshPublicKeys = @(
-    "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAiHq57Mo7efkA05q33JkdZ9g96VE4TjCn8lW0jZxn+n0TzkmlNZEi1E6fbfRSv3iK2XnNBFbOUBLinnMtzmDIAbez0FjKJOSyEk3ZvhD6QAvWh4UW77udzr1V9BROKDbe0ZpkHBHs4nc1LrjZ7+oAVnOHDpYa8FQh/jPf77js11YdNrrPbxi2Gg9SLpcDN6b8L88/eebWDaGNYzKw534eY7JT7FTUwcpAd0krfyh7h99pGJaWtzvwsot/ntQE0QiCmu2IXIYXz0iKBuI38PD9AAR3l7vsOzHIkWqcTRhNsfcrlvST8lZcrlOfwdK8peu1RGRegvWeL8tvunAd9rjBNQ== agitech"
+  "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAiHq57Mo7efkA05q33JkdZ9g96VE4TjCn8lW0jZxn+n0TzkmlNZEi1E6fbfRSv3iK2XnNBFbOUBLinnMtzmDIAbez0FjKJOSyEk3ZvhD6QAvWh4UW77udzr1V9BROKDbe0ZpkHBHs4nc1LrjZ7+oAVnOHDpYa8FQh/jPf77js11YdNrrPbxi2Gg9SLpcDN6b8L88/eebWDaGNYzKw534eY7JT7FTUwcpAd0krfyh7h99pGJaWtzvwsot/ntQE0QiCmu2IXIYXz0iKBuI38PD9AAR3l7vsOzHIkWqcTRhNsfcrlvST8lZcrlOfwdK8peu1RGRegvWeL8tvunAd9rjBNQ== agitech"
 )
 
 function _PwshContextMenu {
-    #$oldVarDefault = (Get-ItemProperty -path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\Open\Command)."(Default)"
-    #$oldVarDefautValue = '%systemroot%\system32\WindowsPowerShell\v1.0\powershell.exe -Command "&''%1''"'
+  #$oldVarDefault = (Get-ItemProperty -path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\Open\Command)."(Default)"
+  #$oldVarDefautValue = '%systemroot%\system32\WindowsPowerShell\v1.0\powershell.exe -Command "&''%1''"'
   
-    $pwshVarMenu = "$((Get-Command pwsh).Path) -Command ""&'%1'"""
+  $pwshVarMenu = "$((Get-Command pwsh).Path) -Command ""&'%1'"""
   
-    Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\Open\Command -Type ExpandString -Name '(Default)' -Value $pwshVarMenu
+  Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\Open\Command -Type ExpandString -Name '(Default)' -Value $pwshVarMenu
   
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64 -Force
-    Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64 -Name "Icon" -Value $pwshPath -Type String
-    Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64 -Name "MUIVerb" -Value "Run with PowerShell 7" -Type String
-    New-Item -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64\Command -Force
-    $keyValue = 'C:\Program Files\PowerShell\7\pwsh.exe -Command "$host.UI.RawUI.WindowTitle = ''PowerShell 7 (x64)''; if((Get-ExecutionPolicy ) -ne ''AllSigned'') { Set-ExecutionPolicy -Scope Process Bypass }; & ''%1''"' 
-    Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64\Command -Name '(Default)' -Value $keyValue -Type String
-    Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64\Command -Name "PowerShellPath" -Value $pwshPath -Type String
+  New-Item -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64 -Force
+  Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64 -Name "Icon" -Value $pwshPath -Type String
+  Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64 -Name "MUIVerb" -Value "Run with PowerShell 7" -Type String
+  New-Item -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64\Command -Force
+  $keyValue = 'C:\Program Files\PowerShell\7\pwsh.exe -Command "$host.UI.RawUI.WindowTitle = ''PowerShell 7 (x64)''; if((Get-ExecutionPolicy ) -ne ''AllSigned'') { Set-ExecutionPolicy -Scope Process Bypass }; & ''%1''"' 
+  Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64\Command -Name '(Default)' -Value $keyValue -Type String
+  Set-ItemProperty -Path Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\PowerShell7x64\Command -Name "PowerShellPath" -Value $pwshPath -Type String
   
-    #"C:\Program Files\PowerShell\7\pwsh.exe" -WindowStyle Hidden "-Command" ""& {Start-Process """C:\Program Files\PowerShell\7\pwsh.exe""" -ArgumentList '-ExecutionPolicy RemoteSigned -File \"%1\"' -Verb RunAs;start-sleep 1}"
-    $keyName = "Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\Run with PowerShell 7 (Admin)"
-    New-Item -Path $keyName -Force
-    Set-ItemProperty -Path $keyName -Name "Icon" -Value $pwshPath -Type String
-    Set-ItemProperty -Path $keyName "MUIVerb" -Value "Run with PowerShell 7" -Type String
-    New-Item -Path "$keyName\Command" -Force
-    $keyValue = 'C:\Program Files\PowerShell\7\pwsh.exe" -WindowStyle Hidden "-Command" ""& {Start-Process """C:\Program Files\PowerShell\7\pwsh.exe""" -ArgumentList ''-ExecutionPolicy RemoteSigned -File \"%1\"'' -Verb RunAs;start-sleep 1}'
-    Set-ItemProperty -Path "$keyName\Command" -Name '(Default)' -Value $keyValue -Type String
-    Set-ItemProperty -Path "$keyName\Command" -Name "PowerShellPath" -Value $pwshPath -Type String
-  }
+  #"C:\Program Files\PowerShell\7\pwsh.exe" -WindowStyle Hidden "-Command" ""& {Start-Process """C:\Program Files\PowerShell\7\pwsh.exe""" -ArgumentList '-ExecutionPolicy RemoteSigned -File \"%1\"' -Verb RunAs;start-sleep 1}"
+  $keyName = "Registry::HKEY_CLASSES_ROOT\Microsoft.PowerShellScript.1\Shell\Run with PowerShell 7 (Admin)"
+  New-Item -Path $keyName -Force
+  Set-ItemProperty -Path $keyName -Name "Icon" -Value $pwshPath -Type String
+  Set-ItemProperty -Path $keyName "MUIVerb" -Value "Run with PowerShell 7" -Type String
+  New-Item -Path "$keyName\Command" -Force
+  $keyValue = 'C:\Program Files\PowerShell\7\pwsh.exe" -WindowStyle Hidden "-Command" ""& {Start-Process """C:\Program Files\PowerShell\7\pwsh.exe""" -ArgumentList ''-ExecutionPolicy RemoteSigned -File \"%1\"'' -Verb RunAs;start-sleep 1}'
+  Set-ItemProperty -Path "$keyName\Command" -Name '(Default)' -Value $keyValue -Type String
+  Set-ItemProperty -Path "$keyName\Command" -Name "PowerShellPath" -Value $pwshPath -Type String
+}
   
-function _CheckServerConnection
-{
-try{
+function _CheckServerConnection {
+  try {
     Invoke-RestMethod -Uri "$uri/GetConfig";
     return $true;
-}
-catch
-{
+  }
+  catch {
     return $false;
-}
+  }
 }
 
 function Get-IsAdmin {  
-    $Principal = new-object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())
-    [bool]$Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+  $Principal = new-object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())
+  [bool]$Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
 function WriteLog {
-    Param ([string]$LogString)
-    Write-Host $LogString
-    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
-    $LogMessage = "$Stamp $LogString"
-    Add-content $LogFile -value $LogMessage
+  Param ([string]$LogString)
+  Write-Host $LogString
+  $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+  $LogMessage = "$Stamp $LogString"
+  Add-content $LogFile -value $LogMessage
 }
 
 # find modules
 function FindModules {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [string]$ModulesFolder
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [string]$ModulesFolder
+  )
 	
-    $deep = 5;
+  $deep = 5;
 	
-    $folders = New-Object System.Collections.Generic.List[string]
+  $folders = New-Object System.Collections.Generic.List[string]
 	
-	if(!$ModulesFolder){
-		$ModulesFolder = $PSScriptRoot
-	}
+  if (!$ModulesFolder) {
+    $ModulesFolder = $PSScriptRoot
+  }
 
-    for($i=0; $i -le $deep; $i++){
-        $folders.Add("$ModulesFolder\$('..\'*$i)Modules");
+  for ($i = 0; $i -le $deep; $i++) {
+    $folders.Add("$ModulesFolder\$('..\'*$i)Modules");
+  }
+  foreach ($item in $folders) {
+    if (Test-Path $item -PathType Container) {
+      $modulePathBase = $item;
+      break;
     }
-    foreach($item in $folders)
-    {
-        if(Test-Path $item -PathType Container){
-            $modulePathBase = $item;
-            break;
-        }
-    }
+  }
 
-    $pathArray = $( (Resolve-Path "$modulePathBase\Agt.Common\Public\").Path, `
-        (Resolve-Path "$modulePathBase\Agt.Install\Public\").Path, `
-        (Resolve-Path "$modulePathBase\Agt.Network\").Path)
+  $pathArray = $( (Resolve-Path "$modulePathBase\Agt.Common\Public\").Path, `
+    (Resolve-Path "$modulePathBase\Agt.Install\Public\").Path, `
+    (Resolve-Path "$modulePathBase\Agt.Network\").Path)
 
-    foreach ($path in $pathArray) {
-        foreach ($item in (Get-ChildItem "$path\*.ps1")) {
-            . "$($item.FullName)"
-        }
+  foreach ($path in $pathArray) {
+    foreach ($item in (Get-ChildItem "$path\*.ps1")) {
+      . "$($item.FullName)"
     }
+  }
 }
 
 ########  Variables
@@ -179,80 +176,72 @@ function FindModules {
 $thisFileName = $MyInvocation.MyCommand.Name
 #$thisFileFullName = $MyInvocation.MyCommand.Path
 $scriptFile = [System.IO.Path]::Combine($PSScriptRoot, $thisFileName)
-$replacements = @{"ScriptFile" = "'$scriptFile' -Watch"}
+$replacements = @{"ScriptFile" = "'$scriptFile' -Watch" }
 #$debugger = $false; #($PSBoundParameters.ContainsKey("Debug")) -or ($DebugPreference  -eq "SilentlyContinue")
 $taskName = "PwshWatcher"
-$publicKeys = @("AAAAB3NzaC1yc2EAAAABJQAAAQEAiHq57Mo7efkA05q33JkdZ9g96VE4TjCn8lW0jZxn+n0TzkmlNZEi1E6fbfRSv3iK2XnNBFbOUBLinnMtzmDIAbez0FjKJOSyEk3ZvhD6QAvWh4UW77udzr1V9BROKDbe0ZpkHBHs4nc1LrjZ7+oAVnOHDpYa8FQh/jPf77js11YdNrrPbxi2Gg9SLpcDN6b8L88/eebWDaGNYzKw534eY7JT7FTUwcpAd0krfyh7h99pGJaWtzvwsot/ntQE0QiCmu2IXIYXz0iKBuI38PD9AAR3l7vsOzHIkWqcTRhNsfcrlvST8lZcrlOfwdK8peu1RGRegvWeL8tvunAd9rjBNQ==")
-
 if (Get-IsAdmin) {
-    try {
-        WriteLog "Finding modules ..."
-        . FindModules
-        WriteLog "Modules finded successfully."
+  try {
+    WriteLog "Finding modules ..."
+    . FindModules
+    WriteLog "Modules finded successfully."
 
-        if($Init){
+    if ($Init) {
           
-          if(Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue)
-          {
-            Unregister-ScheduledTask -TaskName $taskName
-          }
+      if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
+        Unregister-ScheduledTask -TaskName $taskName
+      }
 
-          exit
-        }
+      exit
+    }
 
-        $result = Register-Task -TaskData $TasksDefinitions[$taskName] -Replacements $replacements          
+    $result = Register-Task -TaskData $TasksDefinitions[$taskName] -Replacements $replacements          
 
-        WriteLog "Installing pwsh ..."
-        Install-Powershell
+    WriteLog "Installing pwsh ..."
+    Install-Powershell
 
-        WriteLog "Installing far ..."
-        Install-Far
+    WriteLog "Installing far ..."
+    Install-Far
 
-        WriteLog "Installing ssh ..."
-        Install-OpenSsh 
-        
-        WriteLog "Configuring ssh ..."
-        Set-OpenSsh -PublicKeys $publicKeys
-        
-        if($Install)
-        {
-            WriteLog "Configuring ssh ..."
-            Set-OpenSSH -PublicKeys $sshPublicKeys -DisablePassword $true
+    WriteLog "Installing ssh ..."
+    Install-OpenSsh 
+  
+    WriteLog "Configuring ssh ..."
+    Set-OpenSSH -PublicKeys $sshPublicKeys -DisablePassword $true
 
-            # Check services
-            # DNS Client, Function Discovery Provider Host, Function Discovery Resource Publication, HomeGroup Provider, HomeGroup Listener, Peer Networking Grouping, SSDP Discovery, UPnP Device Host
+    # Check services
+    # DNS Client, Function Discovery Provider Host, Function Discovery Resource Publication, HomeGroup Provider, HomeGroup Listener, Peer Networking Grouping, SSDP Discovery, UPnP Device Host
             
-            $items = @("sshd", "dnscache", "fdphost", "FDResPub", "p2psvc", "ssdpsrv", "upnphost")
-            foreach ($item in $items) {
-                if (($service = Get-Service -Name $item -ErrorAction SilentlyContinue)) {
-                    # ($service.StartType) -eq [System.ServiceProcess.ServiceStartMode]::Manual 
-                    if ($service.StartType -ne [System.ServiceProcess.ServiceStartMode]::Automatic) {
-                        $service | Set-Service -StartupType ([System.ServiceProcess.ServiceStartMode]::Automatic)
-                    }
-                    if ($service.Status -ne "Running") {
-                        $service | Start-Service
-                    }
-                    else {
-                        $service | Restart-Service
-                    }
-                }
-            }
+    $items = @("sshd", "dnscache", "fdphost", "FDResPub", "p2psvc", "ssdpsrv", "upnphost")
+    foreach ($item in $items) {
+      if (($service = Get-Service -Name $item -ErrorAction SilentlyContinue)) {
+        # ($service.StartType) -eq [System.ServiceProcess.ServiceStartMode]::Manual 
+        if ($service.StartType -ne [System.ServiceProcess.ServiceStartMode]::Automatic) {
+          $service | Set-Service -StartupType ([System.ServiceProcess.ServiceStartMode]::Automatic)
         }
+        if ($service.Status -ne "Running") {
+          $service | Start-Service
+        }
+        else {
+          $service | Restart-Service
+        }
+      }
     }
-    catch {
-        WriteLog "GetFiles Error: $_"
-        exit
-    }
+
+  }
+  catch {
+    WriteLog "GetFiles Error: $_"
+    exit
+  }
 }
 else {
-    try{
-        pwsh -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden 
-                   -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "$PSCommandPath"' -Verb RunAs}";
-    }
-    catch [System.Management.Automation.CommandNotFoundException] {
-        PowerShell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden `
-                   -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "$PSCommandPath"' -Verb RunAs}";
-    }
+  try {
+    pwsh -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden 
+    -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "$PSCommandPath"' -Verb RunAs}";
+  }
+  catch [System.Management.Automation.CommandNotFoundException] {
+    PowerShell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden `
+      -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "$PSCommandPath"' -Verb RunAs}";
+  }
 }
 
 WriteLog 'All task completed successfully...';
