4452f8450fd61726115e13c9de477fa74a0e58d4
agitech <agitech@gmail.com>
2023-08-29 13:24:37 +0300

    123

--- Modules/Agt.Common/Public/Register-Task.ps1
+++ Modules/Agt.Common/Public/Register-Task.ps1
@@ -4,7 +4,24 @@ function Register-Task {
     .SYNOPSIS
         Is powershell session runned in admin mode 
     .DESCRIPTION
-    .PARAMETER Name
+    .PARAMETER TaskData
+        [hashtable] data for task, key = value
+            "Name"   = task name
+            "Values" =  hashtable for the values to be substituted into the XmlDefinition of the task
+                        before the task is reregistered. Format [xpath to node] = [node value]. Examples:
+                        "/ns:Task/ns:Actions/ns:Exec/ns:Command"   = "D:\temp\funny.exe";
+                        "/ns:Task/ns:Actions/ns:Exec/ns:Argumetns" = "-set=12"
+                        The values for the nodes on the specified xpath will be replaced with the specified ones
+            "XmlDefinition" = xml task definition (can be obtained when exporting a task)
+                        task xml definition contains key <Version>xxx</Version> which can be used to check the task is registred in the Task Scheduller
+                        is actual, if task definition that send has a higher version number than registred in the Task Sheduller
+                        registred task will be unregistred and new will one
+    .PARAMETER Principal
+        [string] one of the set values, which is used as the princiapl when registering the task (not used in the current edition)
+    .PARAMETER OnlyCheck
+        [switch] Just check if task with specified name exist
+    .PARAMETER Force
+        [switch] for future use
     .INPUTS
     .OUTPUTS
     .EXAMPLE
